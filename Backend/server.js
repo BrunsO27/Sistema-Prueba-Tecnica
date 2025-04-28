62e05acbf6f09c4a8b18311c8fd487336fa72b14
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-28 11:08:21 -0600

    Inicializacion del backend

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT
+
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor escuchando en:', this.port);
+        });
+    }
+}
+
+
+module.exports = Server;
